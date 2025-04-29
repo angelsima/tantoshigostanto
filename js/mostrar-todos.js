2d23019faf976fd51663638c3ed13d356514e870
angelsima <asimonmarquez@gmail.com>
2025-04-29 12:20:16 +0200

    Update mostrar-todos.js

--- js/mostrar-todos.js
+++ js/mostrar-todos.js
@@ -1,1 +1,6 @@
-
+// Opcional: Botón "Mostrar todos"
+    const showAllButton = document.createElement('button');
+    showAllButton.textContent = 'Mostrar todos';
+    showAllButton.className = 'subcategory-button';
+    showAllButton.dataset.category = 'all';
+    document.querySelector('.subcategory-buttons').prepend(showAllButton);

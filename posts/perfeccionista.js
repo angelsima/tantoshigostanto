110fcfb85404d948a2894352a602112e9bebeda1
angelsima <asimonmarquez@gmail.com>
2025-05-08 13:41:53 +0200

    Update perfeccionista.js

--- posts/perfeccionista.js
+++ posts/perfeccionista.js
@@ -1,8 +1,23 @@
 // js/posts/perfeccionista.js
 export function initTextoAnimado() {
     const textos = [
-        "Una viej",
-        "Una anciana sale de su casa al",
+       {
+        fijo: "Amanece. Una", // Parte que permanecerá
+        variable: " viej",   // Parte que se borrará
+        borrarVariable: true    // Controlar si se borra esta parte
+    },
+    {
+        fijo: "Amanece.", // Mismo inicio
+        variable: " Una anciana sale de", // Nueva variación
+        borrarVariable: true
+    }, 
+          {
+        fijo: "Amanece. La anciana", // Mismo inicio
+        variable: "  sale de", // Nueva variación
+        borrarVariable: true
+    } 
+        "Amanece. Una viej",
+        "Amanece. Una anciana sale de su casa",
         "Amanece. Mi abuela Juana",
         "Amanece. Asunción sale de su",
         "..",
@@ -25,31 +40,44 @@ export function initTextoAnimado() {
     const pausaEntreTextos = 1500;
 
     function escribirTexto() {
-        const textoActual = textos[indexTexto];
+        const item = textos[indexTexto];
+    const textoCompleto = item.fijo + item.variable;
         
         if (!borrando) {
-            elementoTexto.innerHTML = textoActual.substring(0, indexChar + 1) + '<span class="cursor-parpadeo">|</span>';
-            indexChar++;
-            
-            if (indexChar === textoActual.length) {
+                // Escribir normalmente
+        const textoVisible = textoCompleto.substring(0, indexChar + 1);
+        elementoTexto.innerHTML = textoVisible + '<span class="cursor-parpadeo">|</span>';
+        indexChar++;
+        
+        if (indexChar === textoCompleto.length) {
+            if (item.borrarVariable) {
                 borrando = true;
                 setTimeout(escribirTexto, pausaEntreTextos);
             } else {
-                setTimeout(escribirTexto, velocidadEscritura);
+                // Pasar al siguiente texto sin borrar
+                indexTexto = (indexTexto + 1) % textos.length;
+                indexChar = item.fijo.length; // Empezar después de la parte fija
+                setTimeout(escribirTexto, pausaEntreTextos);
             }
         } else {
-            elementoTexto.innerHTML = textoActual.substring(0, indexChar - 1) + '<span class="cursor-parpadeo">|</span>';
-            indexChar--;
-            
-            if (indexChar === 0) {
-                borrando = false;
-                indexTexto = (indexTexto + 1) % textos.length;
-                setTimeout(escribirTexto, velocidadEscritura);
-            } else {
-                setTimeout(escribirTexto, velocidadBorrado);
-            }
+            setTimeout(escribirTexto, velocidadEscritura);
+        }
+    } else {
+        // Borrar solo la parte variable
+        const textoVisible = item.fijo + item.variable.substring(0, indexChar - item.fijo.length);
+        elementoTexto.innerHTML = textoVisible + '<span class="cursor-parpadeo">|</span>';
+        indexChar--;
+        
+        if (indexChar === item.fijo.length) {
+            borrando = false;
+            indexTexto = (indexTexto + 1) % textos.length;
+            indexChar = item.fijo.length; // Mantener posición de la parte fija
+            setTimeout(escribirTexto, velocidadEscritura);
+        } else {
+            setTimeout(escribirTexto, velocidadBorrado);
         }
     }
+}
 
     // Iniciar animación
     escribirTexto();
